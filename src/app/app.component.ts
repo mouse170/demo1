c60aa3c140ce0f68b900082d428aaf1c5b0a563e
Chu Hung Lin <mouse170@gmail.com>
2019-07-16 16:48:15 +0800

    Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,8 @@ export class AppComponent {
   doSearch(str) {
     this.keyword = str;
   }
+
+  doDelete(id){
+    console.log('Delete' + id);
+  }
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -10,9 +10,13 @@ export class ArticleComponent implements OnInit {
   @Input() i = 1;
   @Input() item: any = {};
 
+  @Output() delete = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  deleteArticle() {
+    this.delete.emit(this.item.id);
+  }
 }
